472a752dea5933bad7da88ead455629c7e448ce3
Brian Gomes Bascoy <gomesbascoy@gmail.com>
2014-09-23 15:18:58 -0400

    adding redis

--- service/dsap.js
+++ service/dsap.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 
-//var redis = require("redis");
-//var client = redis.createClient();
+var redis = require("redis");
+var redis_client = redis.createClient(); // XXX using default db!
 
 var sjcl = require('./sjcl');
 var curve = sjcl.ecc.curves.k256;
@@ -20,16 +20,27 @@ http.createServer(function (request, response) {
 			var msg = JSON.parse(packet.msg);
 			console.log("id: "+msg.id);
 
-			var public_key = new sjcl.ecc.ecdsa.publicKey(curve, sjcl.codec.hex.toBits(msg.public_key));
-			var hash = sjcl.hash.sha256.hash(packet.msg);
-			var signature = sjcl.codec.base64.toBits(packet.signature);
-
 			response.writeHeader(200, {'Content-Type': 'text/plain'});
-			if (public_key.verify(hash, signature)) {
-				response.end(JSON.stringify({cookie: "session=1234567890;", redirect: "/"}));
-			} else {
-				response.end("error");
-			}
+			redis_client.get(msg.id, function (err, reply) {
+				if (reply) {
+					// if we already have this id, we verify the signature:
+					console.log("id found!");
+					var public_key = new sjcl.ecc.ecdsa.publicKey(curve, sjcl.codec.hex.toBits(reply));
+					var hash = sjcl.hash.sha256.hash(packet.msg);
+					var signature = sjcl.codec.base64.toBits(packet.signature);
+
+					if (public_key.verify(hash, signature)) {
+						response.end(JSON.stringify({cookie: "session=1234567890;", redirect: "/welcome"}));
+					} else {
+						response.end("error");
+					}
+				} else {
+					// if we don't have the, we add it to the db:
+					console.log("new id!");
+					redis_client.set(msg.id, msg.public_key);
+					response.end(JSON.stringify({cookie: "session=1234567890;", redirect: "/newuser"}));
+				}
+			});
 		});
 	} else {
 		response.writeHead(200, {'Content-Type': 'text/html'});
@@ -39,4 +50,12 @@ http.createServer(function (request, response) {
 		});
 	}
 }).listen(80);
+
+process.on('SIGTERM', terminate);
+process.on('SIGINT', terminate);
+function terminate() {
+	console.log("\nQuitting...");
+	redis_client.quit();
+	process.exit();
+}
 
